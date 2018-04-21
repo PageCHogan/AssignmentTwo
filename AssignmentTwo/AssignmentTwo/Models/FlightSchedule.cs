de5c63cce165edb4ddfbd2f648e23eda8b024744
Unknown <page.hogan@gmail.com>
2018-04-22 08:06:42 +1000

    Parent ticket attempt

---
+++ AssignmentTwo/AssignmentTwo/Models/FlightSchedule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssignmentTwo.Models
+{
+    public class FlightSchedule
+    {
+		public Flight FlightData { get; set; }
+		public Airports AirportData { get; set; }
+
+		public FlightSchedule()
+		{
+			FlightData = new Flight();
+			AirportData = new Airports();
+		}
+    }
+}
