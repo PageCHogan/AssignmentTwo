2884ae2ac7673b458eff962c0ea5870a70b078a3
Unknown <page.hogan@gmail.com>
2018-05-04 06:58:32 +1000

    Targetted DB Access
    Can read/search database and return objects based on keys
    Read: can now save airport objects based on ID field...

---
+++ AssignmentTwo/AssignmentTwo/Services/FlightService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AssignmentTwo.Models;
+
+namespace AssignmentTwo.Services
+{
+    public class FlightService
+    {
+		public Airports LookupAirport(int ID)
+		{
+			Airports result = new Airports();
+
+			//TODO: access database airports table and populate location and code using airport ID provided, return the entire airport to replace the destination / departure of flight before it is saved to DB
+
+			return result;
+		}
+
+	}
+}
